6ed987fd0dcb7e888dc02c3ca07b56617497a683
Saksham Sharma <saksham0808@gmail.com>
2017-01-11 23:05:42 +0530

    Use ENV variable for port

--- docker_run.sh
+++ docker_run.sh
@@ -2,4 +2,4 @@
 
 FOLDER=${MOUNT:-${PWD}/mount}
 
-docker run --rm --privileged -it -p 9998:9998 --net=host -v ${FOLDER}:/pwnpeii/mount pwnpeii:local
+docker run --rm --privileged -it -p ${PORT:-9998}:9998 --net=host -v ${FOLDER}:/pwnpeii/mount pwnpeii:local
